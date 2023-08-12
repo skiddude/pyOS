a6049f5b0986f4d6c71e7c05729c18b46b345677
Prithwis Das <prithwisdas2017@gmail.com>
2023-08-12 13:53:24 +0000

    update

--- programs/login.py
+++ programs/login.py
@@ -7,21 +7,21 @@ def run(shell, args):
     user = input("user: ")
     password = hashlib.sha256(getpass.getpass("password: ").encode()).hexdigest()
 
-    if shell.syscall.correct_password(user, password): # == db(user).password
-        stuff = {
-                'USER': user,
-                'SHELL': 'interpreter',
-                'USERNAME': user,
-                'HOME': '/', # db(user).homedir
-                }
+    #if shell.syscall.correct_password(user, password): # == db(user).password
+    stuff = {
+            'USER': user,
+            'SHELL': 'interpreter',
+            'USERNAME': user,
+            'HOME': '/', # db(user).homedir
+            }
 
-        path = "/" # db(user).homedir
-        newshell = System.new_shell(parent=shell, path=path)
-        add_vars(newshell, stuff)
-        newshell.run()
-    else:
-        shell.stderr.write("Invalid username or password.")
-        shell.stderr.write("")
+    path = "/" # db(user).homedir
+    newshell = System.new_shell(parent=shell, path=path)
+    add_vars(newshell, stuff)
+    newshell.run()
+    #else:
+    #    shell.stderr.write("Invalid username or password.")
+    #    shell.stderr.write("")
 
 def add_vars(shell, stuff):
     for key in stuff:
